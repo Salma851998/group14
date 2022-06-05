67150d354d9a84a5742032261b58a4cb634828a8
Salma851998 <76653375+Salma851998@users.noreply.github.com>
2022-06-05 16:59:43 +0200

    Initial commit

---
+++ task1.php
@@ -0,0 +1,16 @@
+<?php
+ $num1=10;
+ $num2=20;
+ echo "the value of the first number before swap =" .$num1;
+ echo "<br>";
+ echo "the value of the second number before swap =" .$num2;
+echo "<br>";
+ $num1=$num1+$num2;
+ $num2=$num1-$num2;
+ $num1=$num1-$num2;
+
+ echo "the value of the first number after swap =" .$num1;
+ echo "<br>";
+ echo "the value of the second number after swap =" .$num2;
+
+?>
